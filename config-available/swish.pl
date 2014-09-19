2c1a8cb477062ced6cd8ff2fb7616446f96f3666
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-09-19 13:58:24 +0200

    Add SWISH to ClioPatria menu

--- config-available/swish.pl
+++ config-available/swish.pl
@@ -1,10 +1,18 @@
 :- module(conf_swish, []).
+:- use_module(cliopatria(hooks)).
 
 /** <module> Add Prolog interaction to ClioPatria
 */
 
 :- multifile user:file_search_path/2.
 
-user:file_search_path(swish_web, web(.)).
+user:file_search_path(swish_web,      web(.)).
+user:file_search_path(swish_examples, cpacks(swish/examples)).
 
 :- use_module(applications(swish)).
+
+%%      cliopatria:menu_item(-Item, -Label) is nondet.
+%
+%       Add SWISH to the Query menu
+
+cliopatria:menu_item(300=query/swish, 'SWISH Prolog shell').
